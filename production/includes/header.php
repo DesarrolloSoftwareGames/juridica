73c4254b2d0ab025f5c0eb9ebf920e38ae868beb
casierra <casierraf@indracompany.com>
2024-05-13 12:08:08 -0500

    incluye bootstrap global

--- production/includes/header.php
+++ production/includes/header.php
@@ -20,7 +20,9 @@
     <!-- Incluye SweetAlert2 CSS y JS -->
     <link rel="stylesheet" href="../lib/sweetalert2/sweetalert2.min.css">
     <script src="../lib/sweetalert2/sweetalert2.all.min.js"></script>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <style>
         .x_panelm2 {
             margin: 10px 10px 10px 10px;
